533a0607edbedb863abacfd59323f6e0efd4bac6
arindom10 <karindom7@gmail.com>
2024-01-22 14:10:14 +0600

    Card color change

--- src/Components/HomepageComponents/OurTeam.tsx
+++ src/Components/HomepageComponents/OurTeam.tsx
@@ -18,24 +18,11 @@ const TeamMember = ({
   whatsappLink,
   portfolioLink,
 }: Props) => (
-  // <div className="max-w-xs mx-4 my-4 rounded overflow-hidden shadow-lg">
-  //   <Image
-  //     height={100}
-  //     width={100}
-  //     className="w-full h-48 object-cover rounded-full "
-  //     src={imageUrl}
-  //     alt={name}
-  //   />
-  //   <div className="px-6 py-4">
-  //     <div className="font-bold text-xl mb-2">{name}</div>
-  //     <p className="text-gray-700 text-base">{role}</p>
-  //   </div>
-  // </div>
-  <div className="max-w-sm mx-4 my-4 rounded-lg overflow-hidden shadow-lg bg-white transition-transform transform hover:scale-105">
+  <div className="max-w-sm mx-4 my-4 rounded-lg overflow-hidden shadow-lg transition-transform transform hover:scale-105  bg-emerald-100">
     <Image
       height={100}
       width={100}
-      className="w-60 h-48 object-cover rounded-t-lg rounded-full"
+      className="w-60 h-60 object-cover rounded-full bg-white"
       src={imageUrl}
       alt={name}
     />
@@ -69,7 +56,7 @@ const OurTeam = () => {
       name: "Hasibur Rahman",
       role: "Developer",
       imageUrl:
-        "https://i.ibb.co/wdD9mG2/354029548-1838442123216452-7986399071866259178-n-removebg-preview.png",
+        "https://i.ibb.co/Kz5xqt5/354029548-1838442123216452-7986399071866259178-n-removebg-preview.png",
       whatsappLink: "https://wa.me/message/GW4RVNQAFXD3A1",
       portfolioLink: "https://arindomkundu.com/",
     },
@@ -77,13 +64,13 @@ const OurTeam = () => {
       name: "Mehadi Hasan Ridoy",
       role: "Developer",
       imageUrl:
-        "https://i.ibb.co/KXLyKVZ/116868425-2531362697175610-4390450432248232837-n-removebg-preview.png",
+        "https://i.ibb.co/yPDyd4z/116868425-2531362697175610-4390450432248232837-n-removebg-preview.png",
       whatsappLink: "https://wa.me/message/GW4RVNQAFXD3A1",
       portfolioLink: "https://arindomkundu.com/",
     },
     {
-      name: "Mahadi Hasan",
-      role: "UX Designer",
+      name: "Meheady Hasan",
+      role: "UI/UX Designer",
       imageUrl:
         "https://i.ibb.co/qMtf2dr/411635836-1392512394993919-2356123374116143291-n-removebg-preview.png",
       whatsappLink: "https://wa.me/message/GW4RVNQAFXD3A1",
@@ -94,7 +81,7 @@ const OurTeam = () => {
   return (
     <div>
       <div className="flex justify-center">
-        <span className="text-3xl font-bold text-cyan-600">MET OUR TEAM</span>
+        <span className="text-3xl font-bold text-cyan-600">MEET OUR TEAM</span>
       </div>
       <div className="flex flex-wrap justify-center  p-8">
         {teamMembers.map((member) => (
